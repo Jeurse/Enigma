707d2f1c81ee21dcce397a96d0e202bb58e726f1
Jeurse <45465478+Jeurse@users.noreply.github.com>
2018-12-14 16:39:45 +0100

    kl,

--- Programme Enigma.py
+++ Programme Enigma.py
@@ -1,1 +1,56 @@
-lettre = message[0]
+texte=input ("saisir les lettres à remplacer dans l'ordre: ")
+l=len (texte)
+compteur=0
+for i in texte:
+    if i == "a":
+        L1 += 1
+    if i == "b":
+        L2 += 1
+    if i == "c":
+        L3 += 1
+    if i == "d":
+        L4 += 1
+    if i == "e":
+        L5 += 1
+    if i == "f":
+        L6 += 1
+    if i == "g":
+        L7 += 1
+    if i == "h":
+        L8 += 1
+    if i == "i":
+        L9 += 1
+    if i == "j":
+        L10 += 1
+    if i == "k":
+        L11 += 1
+    if i == "l":
+        L12 += 1
+    if i == "m":
+        L13 += 1
+    if i == "n":
+        L14 += 1
+    if i == "o":
+        L15 += 1
+    if i == "p":
+        L16 += 1
+    if i == "q":
+        L17 += 1
+    if i == "r":
+        L18 += 1
+    if i == "s":
+        L19 += 1
+    if i == "t":
+        L20 += 1
+    if i == "u":
+        L21 += 1
+    if i == "v":
+        L22 += 1
+    if i == "w":
+        L23 += 1
+    if i == "x":
+        L24 += 1
+    if i == "y":
+        L25 += 1
+    if i == "z":
+        L26 += 1
