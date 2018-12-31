4b1ff2162f8c62af5935ba699a8c663f0d4428c6
Helene972 <helene.schoonheere@bellevue-lyon.org>
2018-12-31 17:17:53 +0100

    Update Rotor1.py

--- Rotor1.py
+++ Rotor1.py
@@ -1,7 +1,133 @@
-#lettre rentrée : k
+#lettre rentrée : i
 texte=input ("saisir texte: ")
 l=len (texte)
-for k in texte:
-    if k =="a":
-       then k =="v"
-print (k)
+aa="a#jj
+bb="b"#uu
+cc="c"#hh
+dd="d"#ss
+ee="e"#nn
+ff="f"#pp
+gg="g"#vv
+hh="h"#cc
+ii="i"#qq
+jj="j"#ll
+kk="k"#ww
+ll="l"#rr
+mm="m"#dd
+nn="n"#xx
+oo="o"#bb
+pp="p"#tt
+qq="q"#ff
+rr="r"#ii
+ss="s"#zz
+tt="t"#ee
+uu="u"#kk
+vv="v"#oo
+ww="w"#aa
+xx="x"#yy
+yy="y"#gg
+zz="z"#mm
+for i in texte:
+    if i == aa:
+        i,ww = ww,i
+print (i)
+    else:
+        if i == bb:
+            i,oo = oo,i
+print(i)
+    else:
+        if i == cc:
+            i,hh = hh,i
+print(i)
+    else:
+        if i == dd:
+            i,mm = mm,i
+print(i)
+else:
+    if i == ee:
+        i,tt = tt,i
+print(i)
+else:
+    if i == ff:
+        i,qq = qq,i
+print(i)
+else:
+    if i == gg:
+        i,yy = yy,i
+print(i)
+else:
+    if i == hh:
+        i,cc = cc,i
+print(i)
+else:
+    if i == ii:
+        i,rr = rr,i
+print(i)
+else:
+    if i == jj:
+        i,aa = aa,i
+print(i)
+else:
+    if i == kk:
+        i,uu = uu,i
+print(i)
+else:
+    if i == ll:
+        i,jj = jj,i
+print(i)
+else:
+    if i == mm:
+        i,zz = zz,i
+print(i)
+else:
+    if i == nn:
+        i,ee = ee,i
+print(i)
+else:
+    if i == oo:
+        i,vv = vv,i
+print(i)
+else:
+    if i == pp:
+        i,ff= ff,i
+print(i)
+else:
+    if i == qq:
+        i,ii = ii,i
+print(i)
+else:
+    if i == rr:
+        i,ll= ll,i
+print(i)
+else:
+    if i == ss:
+        i,dd = dd,i
+print(i)
+else:
+    if i == tt:
+        i,pp = pp,i
+print(i)
+else:
+    if i == uu:
+        i,bb = bb,i
+print(i)
+else:
+    if i == vv:
+        i,gg = gg,i
+print(i)
+else:
+    if i == ww:
+        i,kk = kk,i
+print(i)
+else:
+    if i == xx:
+        i,nn = nn,i
+print(i)
+else:
+    if i == yy:
+        i,xx = xx,i
+print(i)
+else:
+    if i == zz:
+        i,ss = ss,i
+print(i)
